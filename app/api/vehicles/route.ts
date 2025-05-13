8623b89e117ede5fec18f96293490154e71355de
joaodcp <67809513+joaodcp@users.noreply.github.com>
2025-05-13 13:03:07 +0100

    fix: caching on route handler

--- app/api/vehicles/route.ts
+++ app/api/vehicles/route.ts
@@ -1,14 +1,12 @@
+export const dynamic = "force-static";
+export const revalidate = 10;
+
 export async function GET(request: Request) {
     const res = await fetch(
         "https://cp-rt.up.railway.app/last-vehicles-snapshot",
         {
-            next: {
-                revalidate: 10,
-            },
             headers: {
                 Authorization: "Bearer " + process.env.CP_RT_API_KEY,
-                Pragma: "no-cache",
-                "Cache-Control": "no-cache",
             },
         }
     );
