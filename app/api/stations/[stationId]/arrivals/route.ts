e4045010a75f81a8e68cce21be1d311317ce938d
joaodcp <67809513+joaodcp@users.noreply.github.com>
2026-05-21 23:29:55 +0100

    chore: upgrade next

--- app/api/stations/[stationId]/arrivals/route.ts
+++ app/api/stations/[stationId]/arrivals/route.ts
@@ -2,10 +2,11 @@ export const revalidate = 0;
 
 export async function GET(
     request: Request,
-    { params }: { params: { stationId: string } },
+    { params }: { params: Promise<{ stationId: string }> },
 ) {
+    const { stationId } = await params;
     const res = await fetch(
-        `${process.env.WORKER_BASE_URL}/stations/${params.stationId}/arrivals`,
+        `${process.env.WORKER_BASE_URL}/stations/${stationId}/arrivals`,
         {
             headers: {
                 Authorization: `Bearer ${process.env.WORKER_KEY}`,
--- app/api/trips/[tripNumber]/route.ts
+++ app/api/trips/[tripNumber]/route.ts
@@ -2,10 +2,11 @@ export const revalidate = 0;
 
 export async function GET(
     request: Request,
-    { params }: { params: { tripNumber: string } },
+    { params }: { params: Promise<{ tripNumber: string }> },
 ) {
+    const { tripNumber } = await params;
     const res = await fetch(
-        `${process.env.WORKER_BASE_URL}/trips/${params.tripNumber}`,
+        `${process.env.WORKER_BASE_URL}/trips/${tripNumber}`,
         {
             headers: {
                 Authorization: `Bearer ${process.env.WORKER_KEY}`,
